f19b869bbca6a0fe0501f42b27e3aa99582dab2d
iñaki <luque.126265@e.unavarra.es>
2023-02-21 12:57:18 +0100

    StringCalculator TDD 1.Method Add(string numbers) with 0,1 or 2 numbers  S2 Test Passed for 0 number

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -2,10 +2,15 @@
 
 namespace Deg540\PHPTestingBoilerplate;
 
+use function PHPUnit\Framework\isEmpty;
+
 class StringCalculator
 {
-    function Add(string $numbers):int
+    function Add(String $numbers):int
     {
+        if(empty($numbers)){
+            return 0;
+        }
         return -1;
     }
 }
