b085623b2df25eb402a0bb2f8e6731f13b94d85c
iñaki <luque.126265@e.unavarra.es>
2023-02-21 13:23:15 +0100

    StringCalculator TDD 1.Method Add(string numbers) with 0,1 or 2 numbers  S2 Test Passed for 2 numbers

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -12,7 +12,11 @@ class StringCalculator
             return 0;
         else {
             $numbers_array = explode(",", $numbers);
-            return strval($numbers_array[0]);
+            if(sizeof($numbers_array) == 2){
+                return strval($numbers_array[0])+strval($numbers_array[1]);
+            }else{
+                return strval($numbers_array[0]);
+            }
         }
       
     }
