2705d6a0e73e60203146a07ed6d7fdca0783aa23
iñaki <luque.126265@e.unavarra.es>
2023-02-24 11:40:12 +0100

    StringCalculator TDD 1.Method Add(string numbers) with different delimiters  S2 Test Passed

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -11,7 +11,14 @@ class StringCalculator
         if(empty($addParameters))
             return 0;
         else {
-            $numbersArray = preg_split('/[,|\n]/',$addParameters);
+            if($addParameters[0] =="/" and $addParameters[1] =="/"){
+                list($delimiter, $numbers) = explode("\n",$addParameters,2);
+                $delimiter = substr($delimiter,2);
+                $numbersArray = explode($delimiter, $numbers);
+            }
+            else{
+                $numbersArray = preg_split('/[,|\n]/',$addParameters);
+            }
             $addResult = 0;
             for ($i = 0; $i < sizeof($numbersArray); $i++){
                 $addResult += $numbersArray[$i];
